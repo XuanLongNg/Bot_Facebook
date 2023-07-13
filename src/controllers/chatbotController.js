9c5f5811cdc58d9fa94aae256d787e6889e58250
XuanLong <ngxuanlong2k2@gmail.com>
2023-07-13 14:02:33 +0700

    fix controller

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -3,101 +3,102 @@ const request = require("request");
 dotenv.config();
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  // Send the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 class ChatbotController {
   constructor() {}
   async getHomePage(req, res) {
     return res.send("Start web hooks");
   }
   // Handles messages events
-  handleMessage(sender_psid, received_message) {
-    let response;
 
-    // Checks if the message contains text
-    if (received_message.text) {
-      // Create the payload for a basic text message, which
-      // will be added to the body of our request to the Send API
-      response = {
-        text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
-      };
-    } else if (received_message.attachments) {
-      // Get the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [
-              {
-                title: "Is this the right picture?",
-                subtitle: "Tap a button to answer.",
-                image_url: attachment_url,
-                buttons: [
-                  {
-                    type: "postback",
-                    title: "Yes!",
-                    payload: "yes",
-                  },
-                  {
-                    type: "postback",
-                    title: "No!",
-                    payload: "no",
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      };
-    }
-
-    // Send the response message
-    this.callSendAPI(sender_psid, response);
-  }
-
-  // Handles messaging_postbacks events
-  handlePostback(sender_psid, received_postback) {
-    let response;
-
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-
-    // Set the response based on the postback payload
-    if (payload === "yes") {
-      response = { text: "Thanks!" };
-    } else if (payload === "no") {
-      response = { text: "Oops, try sending another image." };
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
-  }
-  // Sends response messages via the Send API
-  callSendAPI(sender_psid, response) {
-    // Construct the message body
-    let request_body = {
-      recipient: {
-        id: sender_psid,
-      },
-      message: response,
-    };
-
-    // Send the HTTP request to the Messenger Platform
-    request(
-      {
-        uri: "https://graph.facebook.com/v2.6/me/messages",
-        qs: { access_token: PAGE_ACCESS_TOKEN },
-        method: "POST",
-        json: request_body,
-      },
-      (err, res, body) => {
-        if (!err) {
-          console.log("message sent!");
-        } else {
-          console.error("Unable to send message:" + err);
-        }
-      }
-    );
-  }
   getWebhook(req, res) {
     // Parse the query params
     let mode = req.query["hub.mode"];
@@ -129,7 +130,7 @@ class ChatbotController {
       console.log("2");
 
       // Iterate over each entry - there may be multiple if batched
-      body.entry.forEach(function (entry) {
+      body.entry.forEach((entry) => {
         console.log("3");
 
         // Get the webhook event. entry.messaging is an array, but
@@ -144,9 +145,9 @@ class ChatbotController {
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
         if (webhook_event.message) {
-          this.handleMessage(sender_psid, webhook_event.message);
+          handleMessage(sender_psid, webhook_event.message);
         } else if (webhook_event.postback) {
-          this.handlePostback(sender_psid, webhook_event.postback);
+          handlePostback(sender_psid, webhook_event.postback);
         }
       });
 
